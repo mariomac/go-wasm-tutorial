074527c2a368d21a23370dfbddca765ccf2b43b7
Mario Macias <mmacias@newrelic.com>
2020-03-15 22:14:14 +0100

    finished example

--- wasm-main/main.go
+++ wasm-main/main.go
@@ -3,5 +3,18 @@ package main
 import "syscall/js"
 
 func main() {
-	js.Global().Get()
+	window := js.Global()
+	doc := window.Get("document")
+	body := doc.Get("body")
+	div := doc.Call("createElement", "div")
+	div.Set("textContent", "hello!!")
+	body.Call("appendChild", div)
+	body.Set("onclick",
+		js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			div := doc.Call("createElement", "div")
+			div.Set("textContent", "click!!")
+			body.Call("appendChild", div)
+			return nil
+		}))
+	<-make(chan struct{})
 }
